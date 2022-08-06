771897d2a9b5db1f3d86d07350b7891a8e65f6be
Ronak-Raizada <80813283+Ronak-Raizada@users.noreply.github.com>
2022-08-07 02:50:00 +0530

    Add files via upload

---
+++ seleniumPackage/OpenChrome.java
@@ -0,0 +1,24 @@
+// In this selenium code we are opening google website link using chromeDriver
+// Steps to perform 
+// 1. setup the property of chromedriver to open google page through chrome web browser.
+// 2. Initialize Webdriver object through ChromeDriver class.
+// 3. Open the http://www.google.com/ link using get method.
+
+package seleniumPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class OpenChrome {
+
+	public static void main(String[] args) {
+		
+		// 1. setup the property of chromedriver to open google page through chrome web browser.
+		  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe" );
+		  
+		// 2. Initialize Webdriver object through ChromeDriver class.
+	        WebDriver browserObject = new ChromeDriver();
+	        
+		// 3. Open the http://www.google.com/ link using get method.
+	        browserObject.get("http://www.google.com/");
+	}
+
+}
